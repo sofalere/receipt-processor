a7b130ef31a0ff9bb8c57d965adc8d9cbc93e0ce
sofalere <sofalere@gmail.com>
2024-02-20 14:53:09 -0600

    get points using id route

--- src/controllers/points.js
+++ src/controllers/points.js
@@ -1,6 +1,15 @@
+import { getReceiptById } from "../services/inMemoryStorage.js";
+
 const getPoints = (req, res) => {
-  const id = req.params[id];
-  console.log(id);
+  const id = req.params["id"];
+  try {
+    const id = req.params["id"];
+    const receipt = getReceiptById(id);
+    res.json({ points: receipt.points });
+  } catch (error) {
+    console.error('Error fetching points:', error);
+    res.status(500).json({ message: 'Invalid request, please try again' });
+  }
 }
 
 export default getPoints;
--- src/services/inMemoryStorage.js
+++ src/services/inMemoryStorage.js
@@ -2,25 +2,34 @@ const receiptsStorage = [];
 
 const getIdIfReceiptExists = (newReceipt) => {
   let id = false;
+  console.log(receiptsStorage);
 
   receiptsStorage.some((receipt) => {
-    receipt = receipt[Object.keys(receipt)[0]]
     if (receipt.purchaseDate === newReceipt.purchaseDate &&
-            receipt.purchaseTime === newReceipt.purchaseTime &&
-            receipt.total === newReceipt.total) {
-              id = receipt.id;
-              return true;
-            }
+        receipt.purchaseTime === newReceipt.purchaseTime &&
+        receipt.total === newReceipt.total) {
+          id = receipt.id;
+          return true;
+    }
   })
 
   return id;
 }
 
 const saveReceipt = (receipt) => {
-  receiptsStorage.push({ [receipt.id]: receipt});
+  receiptsStorage.push(receipt);
+}
+
+const getReceiptById = (id) => {
+  const receipt = receiptsStorage.find((receipt) => receipt.id === id);
+  if (!receipt) {
+    throw new Error(`Receipt with ID ${id} not found`);
+  }
+  return receipt;
 }
 
 export {
   getIdIfReceiptExists,
-  saveReceipt
+  saveReceipt,
+  getReceiptById
 }
