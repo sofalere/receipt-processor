edde20891de1fd8d1bc188ea9eb9faa2b7d266c6
sofalere <sofalere@gmail.com>
2024-02-20 14:25:23 -0600

    respond with receipt id

--- src/controllers/receipts.js
+++ src/controllers/receipts.js
@@ -5,14 +5,13 @@ import { receiptAlreadySaved, saveReceipt } from "../services/inMemoryStorage.js
 const addReceipt = (req, res) => {
   const receipt = req.body;
   if (receiptAlreadySaved(receipt)) {
-    res.status(400).send('Receipt already exists');
-    return;
+    return res.status(400).json({ message: 'Receipt already exists' });
   }
   const points = calculatePoints(receipt);
   const processedReceipt = processReceipt(receipt, points);
   saveReceipt(processedReceipt);
 
-  res.send('receipt recevied, heres the id');
+  res.json({id: processedReceipt.id});
 };
 
 export default addReceipt;
